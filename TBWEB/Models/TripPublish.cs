04b8eed51e661b70b8c1b2f824bea2310613a5c5
Martyncho <martyncho@hotmail.com>
2014-05-03 09:42:50 -0300

    tb6

---
+++ TBWEB/Models/TripPublish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TBWeb.Models
+{
+    public class TripPublish
+    {
+        public int id { get; set; }
+        public int MyProperty { get; set; }
+
+    }
+}
